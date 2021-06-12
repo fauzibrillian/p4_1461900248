4b2eaa512cd76c4a3fce5d68b3e3f58c0e6aafa1
fauzibrillian <brillianfauzi@gmail.com>
2021-06-12 19:11:18 +0700

    fauzi

---
+++ resources/view/0248.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Master Dokter</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="col-md-12">
+            <div style="height: 15px;"></div>
+            <div class="row">
+                <a href="/dokter/export/" class="btn btn-warning">Export Excel</a>
+                <div style="width: 10px;"></div>
+                <button type="button" class="btn btn-primary mr-5" data-toggle="modal" data-target="#importExcel">
+                    Import Excel
+                </button>
+            </div>
+
+
+            <div class="modal fade" id="importExcel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <form method="post" action="/dokter/import" enctype="multipart/form-data">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Import Excel</h5>
+                            </div>
+                            <div class="modal-body">
+
+                                {{ csrf_field() }}
+
+                                <label>Pilih file excel</label>
+                                <div class="form-group">
+                                    <input type="file" name="file" required="required">
+                                </div>
+
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-primary">Import</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+
+            <br>
+            <table class="table">
+                <tr>
+                    <th>No</th>
+                    <th>Nama Dokter</th>
+                    <th>Jabatan</th>
+                    <th>Action</th>
+                </tr>
+                @foreach($dokter as $key => $data)
+                <tr>
+                    <td>{{ $key+1 }}</td>
+                    <td>{{ $data->nama }}</td>
+                    <td>{{ $data->jabatan }}</td>
+                    <td>
+                        <a href="/dokter/{{ $data->id }}/edit/" class="btn btn-info">Update</a>
+                        <form action="{{ route('dokter.destroy', $data->id) }}" method="POST" class="inline-block">
+                            {!! method_field('delete') . csrf_field() !!}
+                            <button type="submit" class="btn btn-danger">
+                                Delete
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </table>
+        </div>
+    </div>
+
+</body>
+
+</html>
